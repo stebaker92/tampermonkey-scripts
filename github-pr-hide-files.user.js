2caf19703e75805312260fee8e42851957ce29a1
Steve Baker <stebaker92@users.noreply.github.com>
2021-08-26 12:22:19 +0100

    Create github-pr-hide-files.user.js

---
+++ github-pr-hide-files.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         GitHub - Hide test files in PRs
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Collapses all test related files in PRs
+// @author       stebaker92
+// @match        https://github.com/*/*/pull/*
+// @icon         https://github.githubassets.com/pinned-octocat.svg
+// @updateURL    https://github.com/stebaker92/tampermonkey-scripts/raw/master/github-pr-hide-files.user.js
+// @grant        none
+// ==/UserScript==
+
+const testPatterns= [
+    // JS
+    '_test',
+    'spec.js',
+    'fixture',
+];
+
+(function() {
+    'use strict';
+
+    // Init
+    addButton();
+
+    // Pjax
+    document.addEventListener('pjax:end', addButton)
+})();
+
+function toggleTestFiles () {
+    const files = document.querySelectorAll('.file');
+    console.log('got files',files)
+
+    files.forEach(file =>{
+        const path = file.querySelector('.file-header').attributes['data-path'].textContent;
+
+        console.log('checking file',path)
+
+        let isTestFile = false
+        testPatterns.forEach(pattern => {
+            if(path.includes(pattern)){
+                isTestFile = true
+            }
+        })
+
+        console.log(`file is ${isTestFile}`)
+
+        if (isTestFile){
+            const button = file.querySelector('.file-info button')
+
+            if (button.attributes['aria-expanded'].textContent === 'true') {
+                button.click();
+            }
+        }
+    });
+}
+
+function addButton(){
+    var e = document.querySelector('#files_bucket .pr-toolbar .diffbar > .pr-review-tools')
+    if(!e) return;
+    var r = e.querySelector('.GithubTestHiderButton')
+    if (r) {
+        r.parentElement.removeChild(r)
+    }
+
+    var on = false;
+
+    var a = document.createElement('button')
+    a.classList.add('btn', 'btn-sm', 'btn-outline', 'tooltipped', 'tooltipped-s')
+    a.onclick = () => toggleTestFiles(on)
+    a.setAttribute('rel', 'nofollow')
+    a.setAttribute('aria-label', on ? 'Show test files' : 'Hide test files')
+    a.appendChild(document.createTextNode('🧪'))
+
+    var g = document.createElement('div')
+    g.classList.add('GithubTestHiderButton', 'diffbar-item')
+    g.appendChild(a)
+
+    e.insertBefore(g, e.firstChild)}
