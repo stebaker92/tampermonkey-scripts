d08e140d356e9984027c4e50329d840e247e002e
Steve Baker <stebaker92@users.noreply.github.com>
2021-10-04 21:16:56 +0100

    Update twitch-redeem-auto-collector.user.js

--- twitch-redeem-auto-collector.user.js
+++ twitch-redeem-auto-collector.user.js
@@ -2,7 +2,7 @@
 // @name         Twitch reward redeemer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Auto redeems channel awards in Twitch
+// @description  Auto redeems channel rewards in Twitch
 // @author       stebaker92
 // @icon         https://www.google.com/s2/favicons?domain=twitch.tv
 // @updateURL    https://github.com/stebaker92/tampermonkey-scripts/raw/master/twitch-redeem-auto-collector.user.js
@@ -18,7 +18,7 @@
     var targetNodeRoot = document.getElementById('root');
 
     var callbackRoot = function(mutationsList, observer) {
-        // This can definitely be more effiecient but it works! 
+        // This can definitely be more efficient but it works! 
         $(".claimable-bonus__icon") && $(".claimable-bonus__icon").click();
     };
   
@@ -26,6 +26,6 @@
 
     var config = { attributes: false, childList: true, subtree: true };
 
-  // Start observing the target node for DOM mutations.
+    // Start observing the target node for DOM mutations.
     observerroot.observe(targetNodeRoot, config);
 })();
