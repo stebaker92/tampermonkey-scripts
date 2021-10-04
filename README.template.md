4cd3b96fc706a677371bab4c0a94a41d58b6a9d7
Steve Baker <stebaker92@users.noreply.github.com>
2021-10-04 20:48:04 +0100

    Readme generate via GitHub actions (#1)

---
+++ README.template.md
@@ -0,0 +1,5 @@
+# Tampermonkey User Scripts
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
+
+{projects}
---
+++ readme-updater.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const files = fs.readdirSync("./");
+const userscripts = files.filter(f => f.includes('user.js'));
+
+let readmeMarkdown = fs.readFileSync('README.template.md').toString();
+
+let projectsMarkdown = '';
+
+const findField = (js, field) =>  js.find(line => line.includes(`@${field}`))?.replace(`// @${field}`,"").trim();
+
+userscripts.forEach(path => {
+    const js = fs.readFileSync(path).toString().split("\n")
+
+    const name = findField(js, 'name')
+    const description = findField(js, 'description')
+    const url = path && `${path}?raw=1`
+    const version = findField(js, 'version')
+    const imageUrl = version && `https://img.shields.io/badge/v${version}-install-success`
+
+    projectsMarkdown += `
+## ${name}
+[![Install](${imageUrl})](${url})
+${description}
+`;
+
+})
+
+const updatedReadme = readmeMarkdown.replace('{projects}', projectsMarkdown)
+
+fs.writeFileSync('README.md', updatedReadme)
