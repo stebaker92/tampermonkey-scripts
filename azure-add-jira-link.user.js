98106d6dc03fb78d05f036bd992c37d39a54aa6c
Steve Baker <stebaker92@users.noreply.github.com>
2022-04-08 16:42:09 +0100

    Create azure-add-jira-link.user.js

---
+++ azure-add-jira-link.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Azure DevOps - Add Jira Link
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       stebaker92
+// @match        https://*.visualstudio.com/*/_git/*/pullrequest/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=visualstudio.com
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const JIRA_URL = GM_getValue("jira_url") || prompt("What is your JIRA URL?", "https://EXAMPLE.atlassian.net/browse/");
+    GM_setValue("jira_url", JIRA_URL);
+
+    if (!JIRA_URL) return;
+
+    const branch = document.querySelector(".pr-header-branches .bolt-link").innerText;
+    // TODO - regex this: ticketExample = "PXC-1371";
+    const ticketParsed = branch.split("-").slice(0,2).join("-").replace("feature/", "")
+
+    const newEl = document.createElement("a")
+    newEl.innerText = "JIRA";
+    newEl.classList = "bolt-split-button-main bolt-button enabled primary bolt-focus-treatment";
+    newEl.href= JIRA_URL + ticketParsed;
+
+    const header = document.querySelector(".repos-pr-title-row");
+    header.insertAdjacentElement("afterend", newEl);
+
+    // TODO - we could load the JIRA page and append the title onto the PR using GM_xmlHttpRequest
+})();
