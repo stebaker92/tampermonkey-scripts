6ad0403562231f0e319aa4a77aa7d6c89fd7241e
Steve Baker <stebaker92@users.noreply.github.com>
2022-11-02 10:34:58 +0000

    Fix timing issues with dataProviders

--- utils/azure-devops-helper.js
+++ utils/azure-devops-helper.js
@@ -1,33 +1,31 @@
-console.info("Loaded azure-devops-helper")
-
-pageData = getDataProviders();
+console.info("Loaded azure-devops-helper");
 
 function getDataProviders() {
     return dataProviders?.data; // Can also use JSON.parse(document.getElementById('dataProviders')?.innerHTML)?.data;
 }
 
 function getBaseApiUrl() {
-    return azdoApiBaseUrl = `${window.location.origin}${pageData['ms.vss-tfs-web.header-action-data'].suiteHomeUrl}`;
+    return `${window.location.origin}${dataProviders?.data['ms.vss-tfs-web.header-action-data'].suiteHomeUrl}`;
 }
 
 function getOrg() {
-    return pageData["ms.vss-web.page-data"].hostName;
+    return dataProviders?.data["ms.vss-web.page-data"].hostName;
 }
 
 function getProject() {
-    return pageData['ms.vss-tfs-web.page-data'].project.name;
+    return dataProviders?.data['ms.vss-tfs-web.page-data'].project.name;
 }
 
 function getRepo() {
-    return pageData["ms.vss-code-web.versioncontrol-viewmodel-data-provider"].gitRepository.name;
+    return dataProviders?.data["ms.vss-code-web.versioncontrol-viewmodel-data-provider"].gitRepository.name;
 }
 
 function getBranchesUrl() {
-    return pageData['ms.vss-code-web.branches-hub'];
+    return dataProviders?.data['ms.vss-code-web.branches-hub'];
 }
 
 function getBranchesUrl() {
-    return pageData["ms.vss-code-web.versioncontrol-viewmodel-data-provider"]?.gitRepository.url + "/stats/branches"
+    return dataProviders?.data["ms.vss-code-web.versioncontrol-viewmodel-data-provider"]?.gitRepository.url + "/stats/branches"
 }
 
 async function fetchBranches(repoId) {
@@ -41,3 +39,4 @@ azure = {
     getRepo,
     fetchBranches,
 };
+
