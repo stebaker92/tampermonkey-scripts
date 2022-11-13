8f0713ea5b5fd1e3b29fc63d3bf6eac805bca944
Steve Baker <stebaker92@users.noreply.github.com>
2022-11-13 12:57:14 +0000

    Update spotify-hide-podcast-recommendations.user.js

--- spotify-hide-podcast-recommendations.user.js
+++ spotify-hide-podcast-recommendations.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Spotify - Hide Podcast Recommendations
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Hide podcasts from homepage
+// @namespace    https://github.com/stebaker92/
+// @version      0.3
+// @description  Hide podcast recommendations from homepage
 // @author       stebaker92
 // @match        https://open.spotify.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=spotify.com
@@ -11,11 +11,11 @@
 // ==/UserScript==
 
 (function HidePodcastsFromHomePage() {
-    waitForKeyElements(".Root__main-view", (mainView) => {
+    waitForKeyElements(".Root__main-view", (el) => {
         console.log("creating MutationObserver");
 
         const observer = new MutationObserver(() => {
-            const elements = document.querySelectorAll(`a[href*="/episode/"], a[href*="/show/"]`);
+            const elements = document.querySelectorAll(`a[href*="/episodes/"], a[href*="/show/"], a[href*="/podcasts/"]`);
 
             for (const element of elements) {
                 let closestParentSection = element.closest("section");
@@ -23,6 +23,6 @@
             }
         });
 
-        observer.observe(mainView, { attributes: true, childList: true, subtree: true });
+        observer.observe(el, { attributes: true, childList: true, subtree: true });
     }, true);
 })();
