b30d52fd2003fbb4ac6db681a7352838dc7d9ea2
Steve Baker <stebaker92@users.noreply.github.com>
2023-06-15 15:46:00 +0100

    Update and rename azure-hide-archived-repos.user.js to azure-hide-archived-resources.user.js

--- azure-hide-archived-resources.user.js
+++ azure-hide-archived-resources.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Azure DevOps - Hide Archived Repos
+// @name         Azure DevOps - Hide Archived Resources
 // @namespace    https://github.com/stebaker92/
-// @version      0.1
-// @description  Mute repositories with 'archived' in the name
+// @version      0.1.1
+// @description  Mute resources repositories with 'archived' in the name
 // @author       stebaker92
 // @match        https://dev.azure.com/**
 // @match        https://*.visualstudio.com/**
@@ -13,7 +13,13 @@
 (function() {
     'use strict';
 
-    // DevOps uses client side routing so we need to wait until we're on a PR page to inject this button
+    hideArchivedRepos();
+    hideArchivedProjects();
+})();
+
+
+function hideArchivedRepos() {
+    // DevOps uses client side routing so we need to wait until we're on a PR page to run this
     waitForKeyElements(`.repository-selector-callout .bolt-list-cell`, (e) => hideEl(e), false, 100);
 
     function hideEl(el) {
@@ -22,4 +28,8 @@
             el.style.opacity=".4";
         }
     }
-})();
+}
+
+function hideArchivedProjects() {
+    // TODO
+}
